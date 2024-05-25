4616e248d75c6fba849a8724d96f6ef626bf4b55
Ting-Hsuan Huang <felixhuang07@gmail.com>
2024-05-25 19:10:35 +0800

    add ui-manager for menu

---
+++ assets/scripts/menu/ui-manager.ts
@@ -0,0 +1,45 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class UIManager extends cc.Component {
+
+    @property(cc.Button)
+    startButton: cc.Button = null;
+
+    @property(cc.Button)
+    leaderboardButton: cc.Button = null;
+
+    onLoad() {
+        const startClickEventHandler = new cc.Component.EventHandler();
+        startClickEventHandler.target = this.node;
+        startClickEventHandler.component = "ui-manager"
+        startClickEventHandler.handler = "loadGameScene";
+
+        this.startButton.clickEvents.push(startClickEventHandler);
+
+        const leaderboardClickEventHandler = new cc.Component.EventHandler();
+        leaderboardClickEventHandler.target = this.node;
+        leaderboardClickEventHandler.component = "ui-manager"
+        leaderboardClickEventHandler.handler = "loadLeaderboardScene";
+
+        this.leaderboardButton.clickEvents.push(leaderboardClickEventHandler);
+    }
+
+    start() {
+    }
+
+    loadGameScene() {
+        cc.director.loadScene("Level-1");
+    }
+
+    loadLeaderboardScene() {
+        cc.director.loadScene("Leaderboard");
+    }
+}
